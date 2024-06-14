08bbe7b90fe24b773dbe575e1150f16fee8c1e68
glitzsiva <60433592+glitzsiva@users.noreply.github.com>
2024-06-14 20:20:36 +0900

    chore: starter conf removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,6 @@ import UnstyledLink from '@/components/links/UnstyledLink';
  */
 import Logo from '~/svg/Logo.svg';
 
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
-
 export default function HomePage() {
   return (
     <main>
@@ -62,7 +58,7 @@ export default function HomePage() {
           <footer className='absolute bottom-2 text-gray-700'>
             © {new Date().getFullYear()} By{' '}
             <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-              Theodorus Clarence
+             Copyrights
             </UnderlineLink>
           </footer>
         </div>
